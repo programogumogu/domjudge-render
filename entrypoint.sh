ece18cf09e39b9cd960e88708978756d5bad53d1
programogumogu <shiruxiaolin23@gmail.com>
2026-08-08 13:57:04 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,8 +22,8 @@ return [
 ];
 EOF
 
-# 初期 DB セットアップ（tarball 版の正しいパス）
-/opt/domjudge/domserver/bin/dj_setup_database -u "$USER" -p "$PASS" -H "$HOST" -d "$DBNAME"
+# 初期 DB セットアップ
+/opt/domjudge/domserver/bin/dj_setup_database -u "$USER" -p "$PASS" -h "$HOST" -d "$DBNAME"
 
 # 管理者アカウント作成
 /opt/domjudge/domserver/bin/dj_admin_user --add admin --password admin || true
