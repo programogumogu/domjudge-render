514bc7d88f2725c40f78195af072889a027248f3
programogumogu <shiruxiaolin23@gmail.com>
2026-08-08 14:28:54 +0900

    Refactor entrypoint.sh for improved service startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# DATABASE_URL を分解（クエリなし前提）
 DBURL="$DATABASE_URL"
 REST="${DBURL#*://}"
 
@@ -10,7 +9,6 @@ PASS="$(echo "$REST" | cut -d: -f2 | cut -d@ -f1)"
 HOST="$(echo "$REST" | cut -d@ -f2 | cut -d/ -f1)"
 DBNAME="$(echo "$REST" | cut -d/ -f2)"
 
-# 正しい場所に db.php を作成
 mkdir -p /opt/domjudge/domserver/etc
 
 cat <<EOF > /opt/domjudge/domserver/etc/db.php
@@ -26,12 +24,9 @@ EOF
 
 DJBIN="/opt/domjudge/domserver/bin/dj_setup_database"
 
-# PostgreSQL モードの正しい呼び出し
 $DJBIN --user="$USER" --password="$PASS" --host="$HOST" --dbname="$DBNAME" install
 
-# 管理者アカウント作成
 /opt/domjudge/domserver/bin/dj_admin_user --add admin --password admin || true
 
-php-fpm8.1
-nginx
-php -S 0.0.0.0:80 -t /opt/domjudge/webroot
+php-fpm8.1 -F &
+nginx -g "daemon off;"
