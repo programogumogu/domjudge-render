f3b93c676de86afed035518de271f7d00c5451f6
programogumogu <shiruxiaolin23@gmail.com>
2026-08-15 11:07:53 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 cd /opt/domjudge/domserver/webapp
 
+# Remove problematic migration
+rm -f /opt/domjudge/domserver/lib/vendor/domjudge/doctrine-migrations/DoctrineMigrations/Version20221004135409.php
+
 echo "Running Doctrine migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
 
