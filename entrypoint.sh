b95466335cd1998141b01b09f309b107874ef955
programogumogu <shiruxiaolin23@gmail.com>
2026-08-15 11:14:42 +0900

    Refactor entrypoint.sh for directory and data loading changes
    Updated the entrypoint script to change the working directory and modify data loading commands.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-cd /opt/domjudge/domserver/webapp
-
-# Remove problematic migration (MySQL does not have lazy_eval_results)
-rm -f /opt/domjudge/domserver/lib/vendor/domjudge/domjudge/doctrine-migrations/DoctrineMigrations/Version20221004135409.php
+cd /opt/domjudge/webroot
 
 echo "Running Doctrine migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
 
-echo "Loading initial data..."
-php bin/console domjudge:load-data --no-interaction
+echo "Loading default data..."
+php bin/console domjudge:load-default-data --no-interaction
 
 echo "Creating admin user..."
 php bin/console domjudge:admin --add admin --password admin
