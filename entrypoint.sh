f62e929cdf8b7b0a3827dfefd71bbe183d505765
programogumogu <shiruxiaolin23@gmail.com>
2026-08-15 10:22:47 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ DBNAME="$(echo "$REST" | cut -d/ -f2)"
 DJBIN="/opt/domjudge/domserver/bin/dj_setup_database"
 
 echo "Initializing database..."
-$DJBIN -u "$USER" -p "$PASS" install
+$DJBIN -u "$USER" -p "$PASS" -h "$HOST" install
 
 echo "Adding admin user..."
 /opt/domjudge/domserver/bin/dj_admin_user --add admin --password admin || true
