df39020fe3e6262dd7674da9d1532f3a7a0b9259
programogumogu <shiruxiaolin23@gmail.com>
2026-08-15 11:53:57 +0900

    Switch to Symfony built-in server for web app
    Replace PHP-FPM and Nginx with Symfony's built-in server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,8 @@ set -e
 
 cd /opt/domjudge/domserver/webapp
 
-echo "Running Doctrine migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
-
-echo "Loading default data..."
 php bin/console domjudge:load-default-data --no-interaction
 
-php-fpm8.1 -F &
-nginx -g "daemon off;"
+# Render が渡す PORT を使って Symfony の Web サーバーを起動
+php -S 0.0.0.0:$PORT -t public
