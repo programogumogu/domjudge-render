e1287282620c6044396d63ace2e2fefc6173ad9b
programogumogu <shiruxiaolin23@gmail.com>
2026-08-15 11:19:41 +0900

    Remove admin user creation from entrypoint.sh
    Removed admin user creation from entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,5 @@ php bin/console doctrine:migrations:migrate --no-interaction
 echo "Loading default data..."
 php bin/console domjudge:load-default-data --no-interaction
 
-echo "Creating admin user..."
-php bin/console domjudge:admin --add admin --password admin
-
 php-fpm8.1 -F &
 nginx -g "daemon off;"
