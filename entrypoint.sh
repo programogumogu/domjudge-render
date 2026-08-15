15b8567f05a8a6dcd5b7657eb4b67d3c266ed967
programogumogu <shiruxiaolin23@gmail.com>
2026-08-15 11:59:45 +0900

    Replace Symfony server with PHP-FPM and Nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ cd /opt/domjudge/domserver/webapp
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console domjudge:load-default-data --no-interaction
 
-# Render が渡す PORT を使って Symfony の Web サーバーを起動
-php -S 0.0.0.0:$PORT -t public
+php-fpm8.1 -F &
+nginx -g "daemon off;"
